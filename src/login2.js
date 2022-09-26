6e58013c45adf09b6c13ffbcc4c2a19924ced799
Dario Cruz Minoza Jr <dminozadev@gmail.com>
2022-09-26 18:25:34 +0800

    updated login2.js remove imports not being used

--- src/login2.js
+++ src/login2.js
@@ -1,12 +1,4 @@
 import { useState } from "react";
-import profilePic from "./assets/profile-picture.jpg";
-import mailIcon from "./assets/Mail.svg";
-
-import twitterIcon from "./assets/twitter-Icon.svg";
-import facebookIcon from "./assets/facebook-icon.svg";
-import githubIcon from "./assets/GitHub-Icon.svg";
-import instagramIcon from "./assets/Instagram-Icon.svg";
-
 import "./App.css";
 import "./Box.css";
 
