cc7781f30fc7e3c1ec5510321a4a87ab27ea91e7
iannico322 <73187566+iannico322@users.noreply.github.com>
2022-09-26 17:51:58 +0800

    login na way sulod

---
+++ src/login2.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ThemeInputField from './components/ThemeInputField';
+import React from 'react';
+
+function login(){
+ return{
+    <h1>Hello</h1>
+ }
+}
+
+export default App;
