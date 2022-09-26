f41cecf03561d4835a22103c9f575ef23597c868
iannico322 <73187566+iannico322@users.noreply.github.com>
2022-09-26 17:59:14 +0800

    Login

--- src/login2.js
+++ src/login2.js
@@ -1,11 +1,21 @@
-import './App.css';
-import ThemeInputField from './components/ThemeInputField';
-import React from 'react';
+import { useState } from "react";
+import profilePic from "./assets/profile-picture.jpg";
+import mailIcon from "./assets/Mail.svg";
 
-function login(){
- return{
-    <h1>Hello</h1>
- }
+import twitterIcon from "./assets/twitter-Icon.svg";
+import facebookIcon from "./assets/facebook-icon.svg";
+import githubIcon from "./assets/GitHub-Icon.svg";
+import instagramIcon from "./assets/Instagram-Icon.svg";
+
+import "./App.css";
+import "./Box.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <h1>Login</h1>
+  );
 }
 
-export default App;
+export default Login;
